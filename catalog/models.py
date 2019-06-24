9b36739687bd648747a580b452f7ad4a826e8b5c
Brendan Dagys <brendandagys@gmail.com>
2019-06-23 23:26:41 -0400

    Updated max_length on the models.

--- catalog/models.py
+++ catalog/models.py
@@ -6,15 +6,15 @@ import datetime
 # Create your models here.
 class Cafeteria(models.Model):
     c_date = models.DateField(blank=False, primary_key=True, verbose_name='Date', default=timezone.now().date())
-    c_time = models.CharField(max_length=10, null=True, blank=True, verbose_name='Time (24 hr.)')
-    c_coordinator = models.CharField(max_length=10, null=True, blank=True, verbose_name='Staff Pool Coordinator')
-    c_main_doors = models.CharField(max_length=30, null=True, blank=True, verbose_name='Main Doors')
-    c_south_patio_doors = models.CharField(max_length=30, null=True, blank=True, verbose_name='South Patio Doors')
-    c_north_patio_doors_1 = models.CharField(max_length=30, null=True, blank=True, verbose_name='North Patio Doors 1')
-    c_north_patio_doors_2 = models.CharField(max_length=30, null=True, blank=True, verbose_name='North Patio Doors 2')
-    c_monitor = models.CharField(max_length=30, null=True, blank=True, verbose_name='Telephone Monitor')
-    c_directors = models.CharField(max_length=60, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
-    c_runners = models.CharField(max_length=60, null=True, blank=True, verbose_name='Command Centre Runner(s)')
+    c_time = models.CharField(max_length=100, null=True, blank=True, verbose_name='Time (24 hr.)')
+    c_coordinator = models.CharField(max_length=100, null=True, blank=True, verbose_name='Staff Pool Coordinator')
+    c_main_doors = models.CharField(max_length=100, null=True, blank=True, verbose_name='Main Doors')
+    c_south_patio_doors = models.CharField(max_length=100, null=True, blank=True, verbose_name='South Patio Doors')
+    c_north_patio_doors_1 = models.CharField(max_length=100, null=True, blank=True, verbose_name='North Patio Doors 1')
+    c_north_patio_doors_2 = models.CharField(max_length=100, null=True, blank=True, verbose_name='North Patio Doors 2')
+    c_monitor = models.CharField(max_length=100, null=True, blank=True, verbose_name='Telephone Monitor')
+    c_directors = models.CharField(max_length=100, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
+    c_runners = models.CharField(max_length=100, null=True, blank=True, verbose_name='Command Centre Runner(s)')
     c_explain = models.NullBooleanField(verbose_name='Explained to Participants')
 
     class Meta:
@@ -35,14 +35,14 @@ class Cafeteria(models.Model):
 
 class East_Lobby(models.Model):
     e_date = models.DateField(blank=True, primary_key=True, verbose_name='Date', default=timezone.now().date())
-    e_time = models.CharField(max_length=10, null=True, blank=True, verbose_name='Time (24 hr.)')
-    e_coordinator = models.CharField(max_length=10, null=True, blank=True, verbose_name='Staff Pool Coordinator')
-    e_main_doors = models.CharField(max_length=30, null=True, blank=True, verbose_name='Main Doors')
-    e_lab_entrance = models.CharField(max_length=30, null=True, blank=True, verbose_name='Lab Entrance')
-    e_ed_entrance = models.CharField(max_length=30, null=True, blank=True, verbose_name='ED Entrance')
-    e_monitor = models.CharField(max_length=30, null=True, blank=True, verbose_name='Telephone Monitor')
-    e_directors = models.CharField(max_length=60, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
-    e_runners = models.CharField(max_length=60, null=True, blank=True, verbose_name='Command Centre Runner(s)')
+    e_time = models.CharField(max_length=100, null=True, blank=True, verbose_name='Time (24 hr.)')
+    e_coordinator = models.CharField(max_length=100, null=True, blank=True, verbose_name='Staff Pool Coordinator')
+    e_main_doors = models.CharField(max_length=100, null=True, blank=True, verbose_name='Main Doors')
+    e_lab_entrance = models.CharField(max_length=100, null=True, blank=True, verbose_name='Lab Entrance')
+    e_ed_entrance = models.CharField(max_length=100, null=True, blank=True, verbose_name='ED Entrance')
+    e_monitor = models.CharField(max_length=100, null=True, blank=True, verbose_name='Telephone Monitor')
+    e_directors = models.CharField(max_length=100, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
+    e_runners = models.CharField(max_length=100, null=True, blank=True, verbose_name='Command Centre Runner(s)')
     e_explain = models.NullBooleanField(verbose_name='Explained to Participants')
 
     class Meta:
@@ -63,13 +63,13 @@ class East_Lobby(models.Model):
 
 class Town_Centre(models.Model):
     t_date = models.DateField(blank=True, primary_key=True, verbose_name='Date', default=timezone.now().date())
-    t_time = models.CharField(max_length=10, null=True, blank=True, verbose_name='Time (24 hr.)')
-    t_coordinator = models.CharField(max_length=10, null=True, blank=True, verbose_name='Staff Pool Coordinator')
-    t_horticultural = models.CharField(max_length=30, null=True, blank=True, verbose_name='Horticultural Entrance')
-    t_town_centre_main_street = models.CharField(max_length=30, null=True, blank=True, verbose_name='Town Centre/Main Street')
-    t_monitor = models.CharField(max_length=30, null=True, blank=True, verbose_name='Telephone Monitor')
-    t_directors = models.CharField(max_length=60, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
-    t_runners = models.CharField(max_length=60, null=True, blank=True, verbose_name='Command Centre Runner(s)')
+    t_time = models.CharField(max_length=100, null=True, blank=True, verbose_name='Time (24 hr.)')
+    t_coordinator = models.CharField(max_length=100, null=True, blank=True, verbose_name='Staff Pool Coordinator')
+    t_horticultural = models.CharField(max_length=100, null=True, blank=True, verbose_name='Horticultural Entrance')
+    t_town_centre_main_street = models.CharField(max_length=100, null=True, blank=True, verbose_name='Town Centre/Main Street')
+    t_monitor = models.CharField(max_length=100, null=True, blank=True, verbose_name='Telephone Monitor')
+    t_directors = models.CharField(max_length=100, null=True, blank=True, verbose_name='Patient/Visitor Director(s)')
+    t_runners = models.CharField(max_length=100, null=True, blank=True, verbose_name='Command Centre Runner(s)')
     t_explain = models.NullBooleanField(verbose_name='Explained to Participants')
 
     class Meta:
