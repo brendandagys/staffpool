ece5a957fda9236a122779dd7e7ee7edce8eddc0
Brendan Dagys <brendandagys@gmail.com>
2019-07-04 23:47:42 -0400

    Finished tutorial 1. Chat working with two windows...

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,15 @@
 # chat/urls.py
-from django.conf.urls import url
-from django.urls import path
+
+# from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
+# Added from video tutorial
+app_name = 'chat'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    # url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'), # Original line from actual tutorial
 ]
