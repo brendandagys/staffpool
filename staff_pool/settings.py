e4a5b222a75e7357f5cf9db8a728107a57c6fc43
Brendan Dagys <brendandagys@gmail.com>
2019-07-08 17:37:54 -0400

    Changed CHANNEL_LAYERS hosts from 6379 back to redis://localhost:6379

--- staff_pool/settings.py
+++ staff_pool/settings.py
@@ -89,7 +89,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': { #                             This will break localhost
-            "hosts": [os.environ.get('REDIS_URL', 6379)],#'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
