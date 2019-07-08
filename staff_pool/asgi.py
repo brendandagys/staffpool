bd1fc6f5a0697e946b9a9df1f63b2cc4de678410
Brendan Dagys <brendandagys@gmail.com>
2019-07-08 18:57:50 -0400

    added a line in asgi.py to try and prevent toomanyconnections error

--- staff_pool/asgi.py
+++ staff_pool/asgi.py
@@ -8,5 +8,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staff_pool.settings")
+os.environ['ASGI_THREADS']="18"
 django.setup()
 application = get_default_application()
