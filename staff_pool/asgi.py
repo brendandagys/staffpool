fadd854d4b625737ec683ae1a93c74abf3f97faf
Brendan Dagys <brendandagys@gmail.com>
2019-07-08 19:17:10 -0400

    trying

--- staff_pool/asgi.py
+++ staff_pool/asgi.py
@@ -8,6 +8,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "staff_pool.settings")
-# os.environ['ASGI_THREADS']="18"
+os.environ['ASGI_THREADS']="5"
 django.setup()
 application = get_default_application()
