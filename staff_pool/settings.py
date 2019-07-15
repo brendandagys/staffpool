9a66a881d4882c9ed2a554581f984e5ac2113f57
Brendan Dagys <brendandagys@gmail.com>
2019-07-14 22:49:31 -0400

    Changed and then stored SECRET_KEY in an environment variable.

--- staff_pool/settings.py
+++ staff_pool/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v0*a68^ik@e$$76(xq+$(9mymrnmtnqltdwty=g8^r$lgjzuu1'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fake_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
